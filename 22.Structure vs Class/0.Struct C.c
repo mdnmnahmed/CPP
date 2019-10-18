d2476343b13b5d9ad79490f99db33fc6def8fa03
Numan Ahmed <39140801+mdnmnahmed@users.noreply.github.com>
2019-10-18 22:49:11 +0530

    Structure vs Class
    Structure vs Class in C++

---
+++ 22.Structure vs Class/0.Struct C.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+struct st{
+	int x;
+};
+
+int main()
+{
+
+	st=10;
+	//printf("%d",obj1.x);
+}
+
+
---
+++ 22.Structure vs Class/1.Struct vs Class.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+/*
+class myClass{
+	int x;
+};
+
+int main()
+{
+	myClass obj1;
+	obj1.x=10;
+}*/
+
+
+struct myClass{
+	int x;
+};
+
+int main()
+{
+	myClass obj1;
+	obj1.x=10;
+	cout<<obj1.x;
+}
+
+
